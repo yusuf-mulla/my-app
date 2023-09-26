ca44e2e61512afe5cf8b51e6a2b23fd378b065b3
yusuf-mulla <124673307+yusuf-mulla@users.noreply.github.com>
2023-09-26 23:35:49 +0530

    26-09-23

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
 
 function App() {
+  const [data, setData] = useState({
+    name: "",
+    age: "",
+    number: "",
+  });
+  console.log(data);
+
+  const handleSubmit = () => {};
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    if (name === "name") {
+      const regex = /^[a-zA-Z\s]*$/;
+      if (!regex.test(value)) {
+        return;
+      }
+    }
+    setData((prevData) => ({
+      ...prevData,
+      [name]: value
+    }));
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="name"
+          value={data.name}
+          placeholder="name"
+          onChange={handleChange}
+        />
+        <input
+          type="number"
+          placeholder="age"
+          onChange={(e) => setData({ ...data, age: e.target.value })}
+        />
+        <input
+          type="number"
+          placeholder="Number"
+          onChange={(e) => setData({ ...data, number: e.target.value })}
+        />
+        <button type="submit">ADD</button>
+      </form>
     </div>
   );
 }
